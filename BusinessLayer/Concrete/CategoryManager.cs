f543da635e00654ca486d4bc4e6834fe777f5ddc
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-02-13 11:59:50 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+
+        public List<Category> getAllBL()
+        {
+            return repository.List();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            if(p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length>=51)
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repository.Insert(p);
+            }
+        }
+    }
+}
